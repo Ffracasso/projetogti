c8095150f787d3bd1219ce7e2ab64be02a186d26
Ffracasso <senaiffracasso@gmail.com>
2025-04-10 11:03:41 -0300

    projeto novo

---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/criar-conta', function () {
+    return view('criar-conta');
+});
+
+Route::post('/salva-conta', function (Request $request) {
+   // dd($request);
+    $user = new User();
+    $user->name = $request->name;
+    $user->email = $request->email;
+    $user->password = $request->password;
+    $user->save();
+    //teste
+    //teste
+    //teste2
+    //teste2
+
+    return "Usuario salvo com sucesso";
+
+})->name('salva-conta');
+
+
+
+Route::get('/teste', function () {
+    return "O código foi testado";
+});
+
+Route::get('/usuario/{nome}', function ($nome) {
+    return "O usuário atual é: ".$nome;
+});
+
+Route::get('/soma/{num1}/{num2}', function ($num1, $num2) {
+    return "A soma é: ". $num1 + $num2;
+});
+
+
